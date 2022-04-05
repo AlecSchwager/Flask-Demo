d825945425086c7d134d4fb4b617f7488ff7b99b
Alec Schwager <alec-schwager@uiowa.edu>
2022-04-04 21:15:26 -0500

    Created Original Template
    Created flask application template with three separate pages, including the home page

--- app.py
+++ app.py
@@ -5,11 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', pageTitle='Flask Server Home Page')
 
 @app.route('/mike')
 def mike():
-    return render_template('mike.html')
+    return render_template('mike.html', pageTitle='About Mike')
+
+@app.route('/alec')
+def alec():
+    return render_template('alec.html', pageTitle='About Alec')
 
 if __name__ == '__main__':
     app.run(debug=True)
